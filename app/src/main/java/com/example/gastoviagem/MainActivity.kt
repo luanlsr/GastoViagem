d5d5f5192f1ce61126ca3c5cdd2ac9a24691cdf2
Luan Ramalho <luanfswd@gmail.com>
2022-07-21 00:47:35 -0300

    projeto iniciado | activity main criada

---
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    private fun calculate() {
+    }
+}
